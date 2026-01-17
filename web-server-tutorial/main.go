e9ac395ca7d0aa4323b35dbec6cb4a3af4ab3dbb
Prosper Coded <enweremproper@gmail.com>
2026-01-17 15:18:19 +0100

    feat(main) - Implement user handling in web server - Add User struct for user data representation - Create POST /users endpoint to accept user data - Validate user data and handle errors

--- web-server-tutorial/main.go
+++ web-server-tutorial/main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
+type User struct {
+	id int 
+	name string
+	age int 
+
+}
+
+
+// for thread safty use sync.Mutex or sync.RWMutex
+
+var cacheMutex sync.RWMutex
 
 func main (){
+
+	users := []User{}
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 		fmt.Print("Hello world")
+	})
+
+	mux.HandleFunc("POST /users", func(w http.ResponseWriter, r *http.Request) {
+		var user User;
+		err := json.NewDecoder(r.Body).Decode(&user)
+
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+
+		if user.name == "" || user.age <= 0 {
+			http.Error(w, "Invalid user data", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println(user)
+
+		users[len(users) + 1] = user
+		w.WriteHeader(http.StatusNoContent)
+
 	})
 
 	fmt.Println("Server is listening at http://localhost:8080")
